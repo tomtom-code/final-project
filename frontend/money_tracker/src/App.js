93b7941935a9ef69c10cb673df854315b7ae6cb5
tomtom-code <tomlai8882000@hotmail.com>
2020-12-08 18:40:02 -0600

    update route

--- frontend/money_tracker/src/App.js
+++ frontend/money_tracker/src/App.js
@@ -1,14 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello, Just do it </h1>
-      </header>
-    </div>
-  );
+import './App.css';
+import LoginPage from './components/LoginPage'
+import LoginForm from './components/LoginForm'
+import SignupForm from './components/SignupForm'
+// import UserUpdateForm from './components/UserUpdateForm'
+// import MainPage from './components/MainPage'
+
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from "react-router-dom";
+
+class App extends Component() {
+  return() {
+    return(
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route 
+              exact 
+              path='/'
+              render={routeProps =>
+              <LoginPage 
+              {...routeProps}/>
+              } 
+            />
+
+            <Route 
+              exact 
+              path='/login'
+              render={routeProps =>
+              <LoginForm 
+              {...routeProps}/>
+              } 
+            />
+
+            <Route 
+              exact 
+              path='/signup'
+              render={routeProps =>
+              <SignupForm 
+              {...routeProps}/>
+              } 
+            />  
+
+
+
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
+ 
 
 export default App;
---
+++ frontend/money_tracker/src/components/LoginForm.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    constructor(){
+        super()
+        this.state = {
+            name: "",
+            password: "",
+            loggedIn: false,
+            currentUser: ""
+        }
+    }
+
+
+    handleChange = (event) => {
+        const { name, value } = event.target
+        
+        this.setState({
+            [name]: value
+        })
+    };
+
+
+    login = (event) => {
+        event.preventDefault()
+        event.target.reset()
+
+        const {name, password} = this.state
+
+        const user = {name, password}
+
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({user})
+        })
+        .then(r => r.json())
+        .then(response => {
+            // The token below will be used as a header for Authorization in your fetches
+            // If you look in application controller we are requesting the header Authorization
+            // Once it is recieved the token is decrypted and access to data is granted
+            localStorage.setItem("token", response.jwt)
+            console.log(response)
+            this.setState({currentUser: response.user.name, loggedIn: true})
+        })
+    }
+
+    greeting = () => {
+        if(this.state.loggedIn){
+            return <h3>Hello {this.state.currentUser}</h3>
+        }else{
+            return <h3>Please Log In</h3>
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+                    <input type="text" name="password" placeholder="Password" onChange={this.handleChange} />
+                    <button type="submit">Submit</button>
+                </form>
+                {this.greeting()}
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
